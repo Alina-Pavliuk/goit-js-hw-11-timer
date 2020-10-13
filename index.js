a7624921a6dd6bcb716b5c639c6dec19e62f7e7d
Alina Pavliuk <alina.alp26@gmail.com>
2020-10-13 13:58:25 +0300

    added countdown timer

---
+++ index.js
@@ -0,0 +1,61 @@
+class CountdownTimer {
+  constructor({selector, targetDate}) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+    
+    this.refs = {
+      days: document.querySelector(`[data-value="days"]`),
+      hours:document.querySelector(`[data-value="hours"]`),
+      mins:document.querySelector(`[data-value="mins"]`),
+      secs:document.querySelector(`[data-value="secs"]`),
+    };
+
+    this.start()
+  };
+
+  calcTime (time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    this.changeEl(days, hours, mins, secs);
+  };
+
+  changeEl(days, hours, mins, secs) {
+    this.refs.days.innerHTML = days;
+    this.refs.hours.innerHTML = hours;
+    this.refs.mins.innerHTML = mins;
+    this.refs.secs.innerHTML = secs;
+  };
+
+  changeDate () {
+    const time = this.targetDate.getTime() - new Date().getTime()
+    if(time > 0) {
+      this.calcTime(time);
+    } else {
+      this.getError()
+    };
+  };
+
+  pad(value) {
+    return String(value).padStart(2, "0");
+  };
+
+  start() {
+    this.changeDate()
+    setInterval(() => {
+      this.changeDate()
+    }, 1000);
+  };
+
+  getError() {
+    document.querySelector(this.selector).innerHTML = 'time is over';
+  }
+}
+
+new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Oct 17, 2020'),
+});
+
